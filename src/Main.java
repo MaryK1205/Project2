0b88a6f3bd3142df29f36e60724ff50ac62ea58f
Мария <kolesnikovamaria169@gmail,com>
2023-11-10 22:16:57 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int current_amount = 100;
-        int addition_amount = 1100;
-        int new_amount;
-        int new_bonus;
-        if (addition_amount>1000) {
-            new_bonus=Math.round(addition_amount/100);
-            new_amount=current_amount+addition_amount+new_bonus;
-            System.out.println(new_amount);
+        int currentAmount = 100;
+        int additionAmount = 1100;
+        int newAmount;
+        int newBonus;
+        if (additionAmount>1000) {
+            newBonus=Math.round(additionAmount/100);
+            newAmount=currentAmount+additionAmount+newBonus;
+            System.out.println(newAmount);
         }
         else {
-            new_amount=current_amount+addition_amount;
-            System.out.println(new_amount);
+            newAmount=currentAmount+additionAmount;
+            System.out.println(newAmount);
         }
 
 
